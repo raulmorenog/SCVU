d71ffda56c040ebe11b358b1291b1e0739faa889
Hugo <hugo.garcia.deandres@gmail.com>
2022-10-15 12:54:38 +0200

    FT a falta de factorizar

--- Solucion_Ecuaciones.m
+++ Solucion_Ecuaciones.m
@@ -64,26 +64,12 @@ p.Cn_deltaA = -0.0012 ; p.Cn_deltaR = 0.0758;
 mu_long = p.m/(p.rhos*p.Sw*p.c/2);
 I_yynd = p.I_yy/(p.rhos*p.Sw*(p.c/2)^3);
 
-Cx_u = p.CT_xu*cos(p.epsilons)-p.Cd_u; 
-Cz_u = -p.CT_xu*sin(p.epsilons)-p.Cl_u; 
-Cm_u = p.Cm_u;
-Cx_alpha = p.Cls-p.Cd_alpha;
-Cz_alpha = -p.Cl_alpha-p.Cds; 
-Cm_alpha = p.Cm_alpha;
-Cx_alphap = 0;
-Cz_alphap = -p.Cl_alphap;
-Cm_alphap = p.Cm_alphap;
-Cz_q = -p.Cl_q; 
-Cm_q = p.Cm_q;
-Cx_deltae = -p.Cd_deltae; 
-Cz_deltae = -p.Cl_deltae; 
-Cm_deltae = p.Cm_deltae;
-Cm_deltaep = p.Cm_deltaep;
-
-Czs = p.Czs;
-Cxs = p.Cxs;
+Cx_u = p.CT_xu*cos(p.epsilons)-p.Cd_u;     Cx_alpha = p.Cls-p.Cd_alpha;     Cx_alphap = 0;                                  Cx_deltae = -p.Cd_deltae;                                   Cxs = p.Cxs;                             
+Cz_u = -p.CT_xu*sin(p.epsilons)-p.Cl_u;    Cz_alpha = -p.Cl_alpha-p.Cds;    Cz_alphap = -p.Cl_alphap;    Cz_q = -p.Cl_q;    Cz_deltae = -p.Cl_deltae;                                   Czs = p.Czs;
+Cm_u = p.Cm_u;                             Cm_alpha = p.Cm_alpha;           Cm_alphap = p.Cm_alphap;     Cm_q = p.Cm_q;     Cm_deltae = p.Cm_deltae;     Cm_deltaep = p.Cm_deltaep;        
+ 
 %% Construcción del Sistema Longitudinal: [A]*{Y}=[b]*{X} ----------> Funciones de Transferencia Longitudinales
-syms s
+syms s z
 sympref('FloatingPointOutput',true)
 
 A=[2*mu_long*s-Cx_u,-Cx_alpha,-Czs;...
@@ -91,20 +77,101 @@ A=[2*mu_long*s-Cx_u,-Cx_alpha,-Czs;...
     -Cm_u,-(Cm_alphap*s+Cm_alpha),I_yynd*s^2-Cm_q*s];
 b=[Cx_deltae;Cz_deltae;Cm_deltaep*s+Cm_deltae];
 
-%Cramer y factorización
-Den=sym2poly(det(A));
+%Cramer y factorización con s ADIMENSIONAL (s gorro) --> FT con s
+%adimensional
+Den=sym2poly(det(A)); %Cuártica de estabilidad Longitudinal
 
 Num_deltae_u=sym2poly(det([b,A(:,2),A(:,3)]));
-TFdeltae_u_nd=tf([Num_deltae_u],[Den]) %Adimensional!!
-TFdeltae_u= TFdeltae_u_nd*tf([p.Us])
-[zeros_u,polos_u,ganancia_u] = tf2zp(Num_deltae_u*p.Us,Den)
-
+TFdeltae_u_nd=tf([Num_deltae_u],[Den]); %Adimensional!!                                                   
+TFdeltae_u= TFdeltae_u_nd*tf([p.Us]); %Dimensional en u, todavia no en s
 
 Num_deltae_alpha=sym2poly(det([A(:,1),b,A(:,3)]));
-TFdeltae_alpha=tf([Num_deltae_alpha],[Den]) 
-[zeros_alpha,polos_alpha,ganancia_alpha] = tf2zp(Num_deltae_alpha,Den)
+TFdeltae_alpha=tf([Num_deltae_alpha],[Den]); 
 
 Num_deltae_theta=sym2poly(det([A(:,1),A(:,2),b]));
-TFdeltae_theta=tf([Num_deltae_theta],[Den]) 
-[zeros_theta,polos_theta,ganancia_theta] = tf2zp(Num_deltae_theta,Den)
+TFdeltae_theta=tf([Num_deltae_theta],[Den]); 
+
+%Desadimensionalización de s (s = z*c/2Us) --> FT con s dimensional
+DenDim = sym2poly(subs(det(A),s,z*p.c/(2*p.Us)));
+
+Num_deltae_uDim = sym2poly(subs(det([b,A(:,2),A(:,3)]),s,z*p.c/(2*p.Us)));
+TFdeltae_uDim = tf([Num_deltae_uDim],[DenDim])*tf([p.Us]);
+
+Num_deltae_alphaDim = sym2poly(subs(det([A(:,1),b,A(:,3)]),s,z*p.c/(2*p.Us)));
+TFdeltae_alphaDim = tf([Num_deltae_alphaDim],[DenDim]);
+
+Num_deltae_thetaDim = sym2poly(subs(det([A(:,1),A(:,2),b]),s,z*p.c/(2*p.Us)));
+TFdeltae_thetaDim = tf([Num_deltae_thetaDim],[DenDim]);
+
+%Paso a forma factorizada
+% [zeros_u,polos_u,ganancia_u] = tf2zp(Num_deltae_uDim*p.Us,DenDim)
+% [zeros_alpha,polos_alpha,ganancia_alpha] = tf2zp(Num_deltae_alphaDim,DenDim)
+% [zeros_theta,polos_theta,ganancia_theta] = tf2zp(Num_deltae_thetaDim,DenDim)
+
+%% Adimensionalización longitudinal y coeficientes
+mu_latdir = p.m/(p.rhos*p.Sw*p.b/2);
+I_xxnd = p.I_xx/(p.rhos*p.Sw*(p.b/2)^3);
+I_zznd = p.I_zz/(p.rhos*p.Sw*(p.b/2)^3);
+I_xznd = p.I_xx/(p.rhos*p.Sw*(p.b/2)^3);
+
+Cy_beta = p.Cy_beta;    Cy_p = p.Cy_p;    Cy_r = p.Cy_r;    Cy_deltaA = p.Cy_deltaA;    Cy_deltaR = p.Cy_deltaR;    Czs = p.Czs;
+Cl_beta = p.Cl_beta;    Cl_p = p.Cl_p;    Cl_r = p.Cl_r;    Cl_deltaA = p.Cl_deltaA;    Cl_deltaR = p.Cl_deltaR;    Cxs = p.Cxs;
+Cn_beta = p.Cn_beta;    Cn_p = p.Cn_p;    Cn_r = p.Cn_r;    Cn_deltaA = p.Cn_deltaA;    Cn_deltaR = p.Cn_deltaR;
+%% Construcción del Sistema Lat-Dir: [C]*{Y}=[D]*{X} ----------> Funciones de Transferencia Lat-Dir
+
+syms s z
+sympref('FloatingPointOutput',true)
+
+C=[2*mu_latdir*s-Cy_beta,Czs-Cy_p*s,2*mu_latdir-Cy_r;...
+    -Cl_beta,I_xxnd*s^2-Cl_p*s,-Cl_r-I_xznd*s;...
+    -Cn_beta,-Cn_p*s-I_xznd*s^2,I_zznd*s-Cn_r];
+D=[0,Cy_deltaR;Cl_deltaA,Cl_deltaR;Cn_deltaA,Cn_deltaR];
+
+%Cramer y factorización con s ADIMENSIONAL (s gorro) --> FT con s
+%adimensional
+DenLat=sym2poly(det(C)); %Cuártica de estabilidad Lat-Dir
+
+Num_deltaA_beta=sym2poly(det([D(:,1),C(:,2),C(:,3)]));    Num_deltaR_beta=sym2poly(det([D(:,2),C(:,2),C(:,3)]));
+TFdeltaA_beta=tf([Num_deltaA_beta],[DenLat]);             TFdeltaR_beta=tf([Num_deltaR_beta],[DenLat]);                                               
+
+Num_deltaA_phi=sym2poly(det([C(:,1),D(:,1),C(:,3)]));     Num_deltaR_phi=sym2poly(det([C(:,1),D(:,2),C(:,3)]));
+TFdeltaA_phi=tf([Num_deltaA_phi],[DenLat]);               TFdeltaR_phi=tf([Num_deltaR_phi],[DenLat]);
+
+Num_deltaA_r=sym2poly(det([C(:,1),C(:,2),D(:,1)]));       Num_deltaR_r=sym2poly(det([C(:,1),C(:,2),D(:,2)]));
+TFdeltaA_r_nd=tf([Num_deltaA_r],[DenLat]);                TFdeltaR_r_nd=tf([Num_deltaR_r],[DenLat]);
+TFdeltaA_r=TFdeltaA_r_nd*tf(2*p.Us/p.b);                  TFdeltaR_r=TFdeltaR_r_nd*tf(2*p.Us/p.b);
+
+%Desadimensionalización de s (s = z*b/2Us) --> FT con s dimensional
+DenLatDim = sym2poly(subs(det(C),s,z*p.b/(2*p.Us)));
+
+Num_deltaA_betaDim = sym2poly(subs(det([D(:,1),C(:,2),C(:,3)]),s,z*p.b/(2*p.Us)));      Num_deltaR_betaDim = sym2poly(subs(det([D(:,2),C(:,2),C(:,3)]),s,z*p.b/(2*p.Us)));
+TFdeltaA_betaDim = tf([Num_deltaA_betaDim],[DenLatDim]);                                TFdeltaR_betaDim = tf([Num_deltaR_betaDim],[DenLatDim]);
+
+Num_deltaA_phiDim = sym2poly(subs(det([C(:,1),D(:,1),C(:,3)]),s,z*p.b/(2*p.Us)));       Num_deltaR_phiDim = sym2poly(subs(det([C(:,1),D(:,2),C(:,3)]),s,z*p.b/(2*p.Us)));
+TFdeltaA_phiDim = tf([Num_deltaA_phiDim],[DenLatDim]);                                  TFdeltaR_phiDim = tf([Num_deltaR_phiDim],[DenLatDim]);
+    
+Num_deltaA_rDim = sym2poly(subs(det([C(:,1),C(:,2),D(:,1)]),s,z*p.b/(2*p.Us)));         Num_deltaR_rDim = sym2poly(subs(det([C(:,1),C(:,2),D(:,2)]),s,z*p.b/(2*p.Us)));
+TFdeltaA_rDim = tf([Num_deltaA_rDim],[DenLatDim])*tf(2*p.Us/p.b);                       TFdeltaR_rDim = tf([Num_deltaR_rDim],[DenLatDim])*tf(2*p.Us/p.b);
+
+
+%Agruparmos las FT en estructuras --> Conclusión de momento: Las TF_Long coinciden con las del libro y las de Roskam, las TF_Lat-Dirc coinciden con
+%las del libro pero no con Roskam porque tiene signos cambiados en algunas derivadas (Cl_deltaR, Cn_deltaR, Cy_deltaR, Cn_p). Falta factorizar guay, diagramas y respuesta
+
+FT_Longitudinales = struct( ...
+    'FTdeltaE_u',TFdeltae_uDim, ...
+    'FTdeltaE_alpha',TFdeltae_alphaDim, ...
+    'FTdeltaE_theta',TFdeltae_thetaDim);
+
+FT_Lateral_Direccionales = struct( ...
+    'FTdeltaA_beta',TFdeltaA_betaDim, ...
+    'FTdeltaA_phi',TFdeltaA_phiDim, ...
+    'FTdeltaA_r',TFdeltaA_rDim, ...
+    'FTdeltaR_beta',TFdeltaR_betaDim, ...
+    'FTdeltaR_phi',TFdeltaR_phiDim, ...
+    'FTdeltaR_r',TFdeltaR_rDim);
+
+
+
+
+
 
