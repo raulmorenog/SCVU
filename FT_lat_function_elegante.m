8ad7435663e533b1be83c0119d8d3cfd521071ad
Abelrp21 <abelruiz23@gmail.com>
2022-11-13 01:22:31 +0100

    SAS

--- FT_lat_function_elegante.m
+++ FT_lat_function_elegante.m
@@ -1,58 +1,4 @@
-%% Obtención de las FT para el Beech 99
-%Una vez finalizada la ejecución todos los datos se almacenan en la
-%estructura FT_lat
-clc
-clear all 
-%close all 
-
-%% Parámetros --> Los guardamos en una estrucutra p
-% Cambios de unidades
-ft2m = 0.3048;      % ft a m
-lb2kg = 0.4536;     % lb a kg
-slg2kg = 14.5939;   % slg a kg 
-deg2rad = pi/180;   % º a rad
-
-% Condición de vuelo
-p.Us = 450*ft2m;
-p.Ms = 0.434;
-p.qs = 128.2*lb2kg/(ft2m^2)*9.81;
-p.rhos = 2*p.qs/(p.Us^2);
-p.alphas = 0*deg2rad; 
-p.epsilons = 0*deg2rad;
-p.thetas = 0*deg2rad;       % Crucero
-g = 9.81;
-
-p.Cls = 0.306; 
-p.Cds = 0.0298; 
-p.CTxs = 0.0298; 
-p.Cms = 0; 
-p.CmTs = 0; 
-
-% Geometría
-p.Sw = 280*ft2m^2;
-p.b = 46*ft2m;
-p.c = 6.5*ft2m;
-p.cdg_c = 0.16;     % Localización del CDG fracción de la cuerda
-
-% Parámetros másicos
-p.m = 11e3*lb2kg;
-p.I_xx = 15.189e3*slg2kg*ft2m^2 ;
-p.I_yy = 20.250e3*slg2kg*ft2m^2;
-p.I_zz = 34.141e3*slg2kg*ft2m^2;
-p.I_xz = 4.371e3*slg2kg*ft2m^2;
-
-p.Czs = -p.m*g*cos(p.thetas)/(p.qs*p.Sw); 
-p.Cxs = -p.Czs*tan(p.thetas);
-
-% Derivadas de estabilidad lateral-direccionales 
-p.Cl_beta = -0.130; p.Cl_p = -0.50; p.Cl_r = 0.14; 
-p.Cy_beta = -0.59; p.Cy_p = -0.19; p.Cy_r = 0.39;
-p.Cn_beta = 0.08; p.Cn_p = 0.019; p.Cn_r = -0.197;  %Cn_p puesto positivo aunque en los datos era negativo.
-p.Cn_Tbeta = 0; 
-p.Cl_deltaA = 0.156; p.Cl_deltaR = -0.0106; 
-p.Cy_deltaA = 0; p.Cy_deltaR = -0.144; 
-p.Cn_deltaA = -0.0012 ; p.Cn_deltaR = 0.0758;
-
+function [FT_lat] = FT_lat_function(p)    
 %% Derivadas de estabilidad y adimensionalización --> los guardamos en la estructura s
 s.mu = p.m/(p.rhos*p.Sw*p.b/2);
 s.I_xxnd = p.I_xx/(p.rhos*p.Sw*(p.b/2)^3);
@@ -153,6 +99,7 @@ FT_lat.fact = struct('deltaA_beta',zpk(FT_lat.nofact{1,1}),...
                  
 %% Cálculo de propiedades de los modos
 [wn, amort, Poles] = damp(FT_lat.fact.deltaA_beta);
+FT_lat.Poles = Poles;
 FT_lat.espiral.wn = wn(1);
 FT_lat.espiral.poles = Poles(1);
 FT_lat.espiral.tau_S = 1/wn(1);
@@ -166,4 +113,3 @@ FT_lat.dutchroll.wn = wn(2);
 FT_lat.dutchroll.period = 2*pi/FT_lat.dutchroll.wn;
 FT_lat.dutchroll.amort = amort(2);
 FT_lat.dutchroll.t12 = -log(2)/real(FT_lat.dutchroll.poles(1)); 
-
